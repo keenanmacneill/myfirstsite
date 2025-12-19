941d4bc4e236f1ec769251ec5339416dfa3d60d3
Keenan MacNeill <macneill.keenan@gmail.com>
2025-12-19 13:56:46 -0500

    Remove commented-out code from app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+//let array1 = [1, 2, 3, 4, 5, 6]
+//let object1 = {
+    //first: 'Keenan',
+    //last: 'MacNeill'
+//}
+
+//for (let x in object1) {
+    //console.log(x + ' ' + object1[x]);
+//}
+
+//for (x=0;x<array1.length;x++){
+    //console.log(x);
+//}
